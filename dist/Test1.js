a59d4236f47e2d0793db9c9a2afc088dc16523cc
p4ny <ktstpy4251@gmail.com>
2025-09-30 15:54:47 +0700

    add Utils

---
+++ dist/Test1.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Utils = require("./Utils");
+if (Utils.add(2, 3) === 5) {
+    console.log(0);
+}
+else {
+    console.log(1);
+}
+//# sourceMappingURL=Test1.js.map
---
+++ dist/Utils.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function helloworld() {
+    console.log("Hello, World!");
+}
+function add(a, b) {
+    return a + b;
+}
+const Utils = {
+    helloworld,
+    add
+};
+module.exports = Utils;
+//# sourceMappingURL=Utils.js.map
